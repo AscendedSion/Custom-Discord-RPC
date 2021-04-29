91f1371d6c13492a8d28b1880f6db3b62e0238da
AscendedSion <80121670+AscendedSion@users.noreply.github.com>
2021-04-29 08:16:06 +0100

    Add files via upload
    Download all of these in a folder. Separating them will not work.

---
+++ index.js
@@ -0,0 +1,16 @@
+var rpc = require("discord-rpc")
+const client = new rpc.Client({ transport: 'ipc' })
+client.on('ready', () => {
+client.request('SET_ACTIVITY', {
+pid: process.pid,
+activity : {
+details : "SION Is Taking Over.",
+assets : {
+large_image : "image",
+large_text : "This Is The New Reality." // THIS WILL SHOW AS "Playing <Status>" from the outisde
+},
+buttons : [{label : "Join Us" , url : "https://discord.gg/TTYaHnWFHf"},{label : "My Youtube",url : "https://www.youtube.com/channel/UCm2kE8GHM_vJDeW2f9Tfbzw"}]
+}
+})
+})
+client.login({ clientId : "835536064448823367" }).catch(console.error);
