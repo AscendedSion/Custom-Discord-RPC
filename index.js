dfc57bc2f825510af5fac85b256f435f2c54e69d
AscendedSion <80121670+AscendedSion@users.noreply.github.com>
2021-10-28 09:32:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ client.on('ready', () => {
 client.request('SET_ACTIVITY', {
 pid: process.pid,
 activity : {
-details : "SION Is Taking Over.",
+details : "Details Here",
 assets : {
 large_image : "image",
-large_text : "This Is The New Reality." // THIS WILL SHOW AS "Playing <Status>" from the outisde
+large_text : "PlayingStatus" // THIS WILL SHOW AS "Playing <Status>" from the outisde
 },
-buttons : [{label : "Join Us" , url : "https://discord.gg/TTYaHnWFHf"},{label : "My Youtube",url : "https://www.youtube.com/channel/UCm2kE8GHM_vJDeW2f9Tfbzw"}]
+buttons : [{label : "hi" , url : "spanner.com"},{label : "hello again",url : "example.com"}]
 }
 })
 })
